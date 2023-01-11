5790999e0e373517c0e213ab1204876d1d047275
ryudg <ryudg95@gmail.com>
2023-01-11 17:21:11 +0900

    edit data
    edit data

--- index.js
+++ index.js
@@ -11,22 +11,18 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const readFile = fs.readFileSync("./public/json/userData.json", "utf-8");
+const readFile = fs.readFileSync("userData.json", "utf-8");
 const jsonData = JSON.parse(readFile);
 
 let userArr = [];
 userArr = [...jsonData];
-let userInfo = {};
 
 let knowledge104 = [];
 let stage = [];
 let clinic = [];
-const knowledgeFile = fs.readFileSync(
-  "./public/json/knowledgeData.json",
-  "utf-8"
-);
-const stageFile = fs.readFileSync("./public/json/stageData.json", "utf-8");
-const clinicFile = fs.readFileSync("./public/json/clinicData.json", "utf-8");
+const knowledgeFile = fs.readFileSync("knowledgeData.json", "utf-8");
+const stageFile = fs.readFileSync("stageData.json", "utf-8");
+const clinicFile = fs.readFileSync("clinicData.json", "utf-8");
 const jsonKnowData = JSON.parse(knowledgeFile);
 const jsonStageData = JSON.parse(stageFile);
 const jsonClinicData = JSON.parse(clinicFile);
@@ -34,27 +30,19 @@ knowledge104 = [...jsonKnowData];
 stage = [...jsonStageData];
 clinic = [...jsonClinicData];
 
-// 현재시간
-const now = new Date().getTime();
-// 시작시간
-const start = new Date(
-  userArr[0].EndYear,
-  userArr[0].EndMonth - 1,
-  userArr[0].EndDay,
-  userArr[0].EndHour,
-  userArr[0].EndMinute
-).getTime();
-// 지난시간(분)
-const pass = Math.floor((now - start) / (1000 * 60));
-
 let achievedbArr = [];
-const achievedbFile = fs.readFileSync("./public/json/achieveDB.json", "utf-8");
+const achievedbFile = fs.readFileSync("achieveDB.json", "utf-8");
 const achievedbData = JSON.parse(achievedbFile);
 achievedbArr = [...achievedbData];
 
+let test = [];
+const testJson = fs.readFileSync("achieveDBv2.json", "utf-8");
+const testData = JSON.parse(testJson);
+test = [...testData];
+
 // ----------splash----------
 app.get("/", function (req, res) {
-  res.render("pages/index.ejs");
+  res.render("pages/index.ejs", { userArr });
 });
 
 // ----------UserName----------
@@ -63,10 +51,9 @@ app.get("/UserName", (req, res) => {
 });
 // ----------InpuUserNameData----------
 app.post("/UserNameData", (req, res) => {
-  console.log(req.body.userName);
-  userInfo.userName = req.body.userName;
-  userInfo.id = 0;
-  userArr.push(userInfo);
+  // console.log(req.body.userName);
+  userArr[0].userName = req.body.userName;
+  userArr[0].id = 0;
 
   // id 중복 제거
   const filterArr = userArr.reduce((acc, current) => {
@@ -78,7 +65,7 @@ app.post("/UserNameData", (req, res) => {
     }
   }, []);
   userArr = filterArr;
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/NoMoreInfo");
 });
 
@@ -87,7 +74,7 @@ app.get("/StartDate", (req, res) => {
   res.render("pages/StartDate.ejs");
 });
 app.post("/StartDateData", (req, res) => {
-  console.log(req.body.StartYear);
+  // console.log(req.body.StartYear);
   if (req.body.StartMonth < 10) {
     req.body.StartMonth = "0" + req.body.StartMonth;
   }
@@ -106,7 +93,7 @@ app.post("/StartDateData", (req, res) => {
   userArr[0].StartHour = req.body.StartHour;
   userArr[0].StartMinute = req.body.StartMinute;
 
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/EndDate");
 });
 
@@ -115,7 +102,7 @@ app.get("/EndDate", (req, res) => {
   res.render("pages/EndDate.ejs");
 });
 app.post("/EndDateData", (req, res) => {
-  console.log(req.body.EndYear);
+  // console.log(req.body.EndYear);
   if (req.body.EndMonth < 10) {
     req.body.EndMonth = "0" + req.body.EndMonth;
   }
@@ -134,7 +121,7 @@ app.post("/EndDateData", (req, res) => {
   userArr[0].EndHour = req.body.EndHour;
   userArr[0].EndMinute = req.body.EndMinute;
 
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/CountPerDay");
 });
 
@@ -145,7 +132,7 @@ app.get("/CountPerDay", (req, res) => {
 app.post("/CountPerDayData", (req, res) => {
   userArr[0].CountPerDay = req.body.CountPerDay;
 
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/Price");
 });
 
@@ -156,7 +143,7 @@ app.get("/Price", (req, res) => {
 app.post("/PriceData", (req, res) => {
   userArr[0].Price = req.body.Price;
 
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/BrithDay");
 });
 
@@ -174,18 +161,82 @@ app.post("/BrithDayData", (req, res) => {
   userArr[0].BrithDayYear = req.body.BrithDayYear;
   userArr[0].BrithDayMonth = req.body.BrithDayMonth;
   userArr[0].BrithDayDay = req.body.BrithDayDay;
-  fs.writeFileSync("./public/json/userData.json", JSON.stringify(userArr));
+  fs.writeFileSync("userData.json", JSON.stringify(userArr));
   res.redirect("/Main");
 });
 
 // ----------main----------
 app.get("/main", (req, res) => {
+  // 현재시간
+  const now = new Date();
+  let nowY = now.getFullYear();
+  let nowM = now.getMonth() + 1;
+  let nowD = now.getDate();
+  // 시작시간
+  const start = new Date(
+    userArr[0].EndYear,
+    userArr[0].EndMonth - 1,
+    userArr[0].EndDay,
+    userArr[0].EndHour,
+    userArr[0].EndMinute
+  );
+  // 지난시간(분)
+  const pass = Math.floor((now - start) / (1000 * 60));
+
+  let stageCount = stage
+    .map((e) => {
+      return e.min <= pass;
+    })
+    .filter((e) => e == true).length;
+
+  if (nowM < 10) {
+    nowM = "0" + nowM;
+  }
+  if (nowD < 10) {
+    nowD = "0" + nowD;
+  }
+
+  const testPrice = test[0].Price;
+  const testDay = test[1].Day;
+  const testCount = test[2].Count;
+  const day = parseInt((now - start) / (60 * 60 * 24 * 1000));
+
+  let testPriceArr = testPrice.filter((e) => {
+    return e.condition <= day * userArr[0].Price;
+  });
+  let testDayArr = testDay.filter((e) => {
+    return e.condition <= day;
+  });
+  let testCountArr = testCount.filter((e) => {
+    return e.condition <= day * userArr[0].CountPerDay;
+  });
+
+  if (testPriceArr.at(-1).date == undefined) {
+    testPriceArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testDayArr.at(-1).date == undefined) {
+    testDayArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testCountArr.at(-1).date == undefined) {
+    testCountArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+
+  let allLength = testPrice.length + testDay.length + testCount.length;
+  let totalLength =
+    testPriceArr.length + testDayArr.length + testCountArr.length;
+
   res.render("pages/main.ejs", {
     userArr,
     knowledge104,
+    achievedbArr,
     stage,
     pass,
-    achievedbArr,
+    stageCount,
+    totalLength,
+    allLength,
+    testPriceArr,
+    testDayArr,
+    testCountArr,
   });
 });
 
@@ -196,7 +247,27 @@ app.get("/NoMoreInfo", (req, res) => {
 
 // stage
 app.get("/stage", function (req, res) {
-  res.render("pages/stage.ejs", { stage, pass });
+  // 현재시간
+  const now = new Date().getTime();
+  // 시작시간
+  const start = new Date(
+    userArr[0].EndYear,
+    userArr[0].EndMonth - 1,
+    userArr[0].EndDay,
+    userArr[0].EndHour,
+    userArr[0].EndMinute
+  ).getTime();
+  // 지난시간(분)
+  const pass = Math.floor((now - start) / (1000 * 60));
+
+  let stageCount = stage
+    .map((e) => {
+      return e.min <= pass;
+    })
+    .filter((e) => e == true).length;
+  // console.log(stageCount);
+
+  res.render("pages/stage.ejs", { stage, pass, stageCount });
 });
 
 // knowledge
@@ -216,7 +287,126 @@ app.get("/symptom", function (req, res) {
 
 // achievement (업적) 페이지
 app.get("/achievement", function (req, res) {
-  res.render("pages/achievement.ejs", { achievedbArr, userArr });
+  const now = new Date();
+  let nowY = now.getFullYear();
+  let nowM = now.getMonth() + 1;
+  let nowD = now.getDate();
+
+  if (nowM < 10) {
+    nowM = "0" + nowM;
+  }
+  if (nowD < 10) {
+    nowD = "0" + nowD;
+  }
+
+  const start = new Date(
+    userArr[0].EndYear,
+    userArr[0].EndMonth - 1,
+    userArr[0].EndDay,
+    userArr[0].EndHour,
+    userArr[0].EndMinute
+  );
+  const testPrice = test[0].Price;
+  const testDay = test[1].Day;
+  const testCount = test[2].Count;
+  const day = parseInt((now - start) / (60 * 60 * 24 * 1000));
+
+  let testPriceArr = testPrice.filter((e) => {
+    return e.condition <= day * userArr[0].Price;
+  });
+  let testDayArr = testDay.filter((e) => {
+    return e.condition <= day;
+  });
+  let testCountArr = testCount.filter((e) => {
+    return e.condition <= day * userArr[0].CountPerDay;
+  });
+
+  if (testPriceArr.at(-1).date == undefined) {
+    testPriceArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testDayArr.at(-1).date == undefined) {
+    testDayArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testCountArr.at(-1).date == undefined) {
+    testCountArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+
+  let allLength = testPrice.length + testDay.length + testCount.length;
+  let totalLength =
+    testPriceArr.length + testDayArr.length + testCountArr.length;
+
+  // console.log(allLength, totalLength);
+
+  fs.writeFileSync("achieveDBv2.json", JSON.stringify(test));
+  res.render("pages/achievement.ejs", {
+    test,
+    totalLength,
+    achievedbArr,
+    userArr,
+    allLength,
+  });
+});
+
+// userinfo
+app.get("/userinfo", async function (req, res) {
+  res.render("pages/userinfo.ejs", { userArr });
+});
+
+app.get("/setting", async function (req, res) {
+  res.render("pages/setting.ejs");
+});
+
+// test
+app.get("/test", function (req, res) {
+  const now = new Date();
+  let nowY = now.getFullYear();
+  let nowM = now.getMonth() + 1;
+  let nowD = now.getDate();
+
+  if (nowM < 10) {
+    nowM = "0" + nowM;
+  }
+  if (nowD < 10) {
+    nowD = "0" + nowD;
+  }
+
+  const start = new Date(
+    userArr[0].EndYear,
+    userArr[0].EndMonth - 1,
+    userArr[0].EndDay,
+    userArr[0].EndHour,
+    userArr[0].EndMinute
+  );
+  const testPrice = test[0].Price;
+  const testDay = test[1].Day;
+  const testCount = test[2].Count;
+  const day = parseInt((now - start) / (60 * 60 * 24 * 1000));
+
+  let testPriceArr = testPrice.filter((e) => {
+    return e.condition <= day * userArr[0].Price;
+  });
+  let testDayArr = testDay.filter((e) => {
+    return e.condition <= day;
+  });
+  let testCountArr = testCount.filter((e) => {
+    return e.condition <= day * userArr[0].CountPerDay;
+  });
+
+  if (testPriceArr.at(-1).date == undefined) {
+    testPriceArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testDayArr.at(-1).date == undefined) {
+    testDayArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+  if (testCountArr.at(-1).date == undefined) {
+    testCountArr.at(-1).date = `${nowY}-${nowM}-${nowD}`;
+  }
+
+  let totalLength =
+    testPriceArr.length + testDayArr.length + testCountArr.length;
+
+  fs.writeFileSync("achieveDBv2.json", JSON.stringify(test));
+  res.render("pages/test.ejs", { test, totalLength });
 });
 
 // ----------listen----------
